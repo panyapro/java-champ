285240c1aa3626909f89de1e66d6f8668d54c891
pavershin <pavel.avershin@kaspi.kz>
2022-08-13 00:16:12 +0600

    init commit

---
+++ java-champ/src/main/java/cup/online/javachamp/JavaChampApplication.java
@@ -0,0 +1,13 @@
+package cup.online.javachamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChampApplication.class, args);
+	}
+
+}
---
+++ java-champ/src/test/java/cup/online/javachamp/JavaChampApplicationTests.java
@@ -0,0 +1,13 @@
+package cup.online.javachamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
